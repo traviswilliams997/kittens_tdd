7209a564ea799077faa088a1214a8110a4c83d90
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-09-04 16:40:47 -0500

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get('kittens' => 'kittens#index')
-  
+  resources :kittens
+
+  #get('kittens' => 'kittens#index')
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "kittens#index"
 end
