757c9e3e42039f6749acb89908a31758fbde562e
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-09-04 14:57:34 -0500

    Refactor model test

--- spec/models/kitten_spec.rb
+++ spec/models/kitten_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Kitten do
+    subject{described_class.new(name: 'Tommy')
+    }
     describe 'validation' do 
         describe 'name' do
             it 'must be present' do
-                kitten = described_class.new
-                expect(kitten).to_not be_valid 
-                kitten.name = 'Tommy'
-                expect(kitten).to be_valid
+                expect(subject).to be_valid 
+                subject.name = nil
+                expect(subject).to_not be_valid
             end 
         end 
     end 
