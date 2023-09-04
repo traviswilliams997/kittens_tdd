ff26b878d89f9761b73ac9999785471ddc1e32f1
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-09-04 14:55:06 -0500

    Edit test to check or name presesnce

--- spec/models/kitten_spec.rb
+++ spec/models/kitten_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Kitten do
             it 'must be present' do
                 kitten = described_class.new
                 expect(kitten).to_not be_valid 
+                kitten.name = 'Tommy'
+                expect(kitten).to be_valid
             end 
         end 
     end 
