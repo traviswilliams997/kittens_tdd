b6e052ad4a0954422ccc3fe36337b8011544673d
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-09-03 23:02:08 -0500

    Create kittens table

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_04_040041) do
+  create_table "kittens", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.integer "cuteness"
+    t.integer "softness"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
